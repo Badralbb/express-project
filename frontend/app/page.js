86e01d7e785222989dce700be393f96da2f82b7e
Badralbb <badralbaigalmaa7@gmail.com>
2024-08-26 20:09:56 +0800

    commit

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,10 @@
+import { AddNewCategory } from "@/components/AddNewCategory";
+import { HeaderNav } from "@/components/HeaderNav";
 
 
 export default function Home() {
   return <main>
-      
+      <HeaderNav/>
+      <AddNewCategory/>
   </main>;
 }
